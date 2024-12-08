6e12690cf876071605b0ee4317d110e48c0c8599
Raul Roman <raul.roman.125323@gmail.com>
2024-12-08 14:23:41 -0600

    verifying keys oauth 2.0

--- app.py
+++ app.py
@@ -75,6 +75,9 @@ else:
 cipher_suite = Fernet(key)
 
 # Configuración de OAuth
+app.logger.debug(f"GOOGLE_CLIENT_ID: {os.getenv('GOOGLE_CLIENT_ID')}")
+app.logger.debug(f"GOOGLE_CLIENT_SECRET: {os.getenv('GOOGLE_CLIENT_SECRET')}")
+
 GOOGLE_CLIENT_ID = os.getenv('GOOGLE_CLIENT_ID')
 GOOGLE_CLIENT_SECRET = os.getenv('GOOGLE_CLIENT_SECRET')
 GOOGLE_DISCOVERY_URL = os.getenv('GOOGLE_DISCOVERY_URL')
